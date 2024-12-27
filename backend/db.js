f321871e79e5d49d7a5cd5e54f300a847589ca66
sinAbhishek <singhabhshek979@gmail.com>
2024-12-27 23:47:00 +0530

    final

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,3 @@
-// Simple in-memory storage for peers and files
 const peers = {}; // { peerId: { socketId, metadata } }
 const files = {}; // { fileHash: { fileName, fileChunks, peers: [peerId, ...] } }
 
