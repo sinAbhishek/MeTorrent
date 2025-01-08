d7dc802be653f58c959efc309720e127d2cb3def
sinAbhishek <singhabhshek979@gmail.com>
2025-01-08 16:16:07 +0530

    added controller

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -4,4 +4,4 @@ const app = express();
 const router = express.Router();
 
 router.post("/chunks/register", register);
-router.get("/chunks/", register);
+router.get("/chunks/:fileHash/chunks", hash);
